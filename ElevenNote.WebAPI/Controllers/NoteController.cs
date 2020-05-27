eb383b530f0d8f483889a4ff3e9ab099f5a9ae31
Sabra Snider <sabralsnider@gmail.com>
2020-05-27 15:11:02 -0400

    Added NoteDetail Model

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -37,5 +37,11 @@ namespace ElevenNote.WebAPI.Controllers
 
             return Ok();
         }
+        public IHttpActionResult Get(int id)
+        {
+            NoteService noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
     }
 }
